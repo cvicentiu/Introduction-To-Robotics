73b59a3f02ba986ff4b44fb5320d6e34820628a4
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2015-05-11 16:55:12 +0000

    Converted to python.

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-
-public class Main {
-	public static void main(String[] argv) {
-		System.out.println("Hello vicus!! Nu am chef de leo!!!1");
-	}
-}
---
+++ worldmap.py
@@ -0,0 +1,8 @@
+
+class World(object):
+  def __init__(self, num_nodes):
+    self.num_nodes = num_nodes
+
+  def 
+
+
